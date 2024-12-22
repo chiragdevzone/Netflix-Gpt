dfcf6e0f8487861fd86d0a5f267a68dcda6038ce
chirag <chiragsankhla55@gmail.com>
2024-12-22 11:12:56 +0530

    initialize the project and set up tailwind configuration

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center text-green-500">
+        Namaste everyone this is Netflix-gpt
+      </h1>
+    </div>
+  );
+}
+
+export default App;
